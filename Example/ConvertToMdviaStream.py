08e00c863fea550db439bb405f8ac8a64adcc8f9
Xinya Zhu <xinya.zhu@aspose.com>
2025-08-05 09:43:28 +0800

    add stream operate cases

---
+++ Example/ConvertToMdviaStream.py
@@ -0,0 +1,17 @@
+import io
+from aspose.cells import Workbook, SaveFormat
+
+# Read the input file into a memory stream
+with open('HelloWorld.xlsx', 'rb') as file:
+    input_stream = io.BytesIO(file.read())
+
+# Create a Workbook object
+workbook = Workbook(input_stream)
+
+# Save to a memory stream
+out_stream = io.BytesIO()
+workbook.save(out_stream, SaveFormat.MARKDOWN)
+
+# Write the memory stream to an output file
+with open('HelloWorld.md', 'wb') as f:
+    f.write(out_stream.getvalue())
