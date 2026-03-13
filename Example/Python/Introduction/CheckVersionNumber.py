2442f37e6d3ea5806ce9dfd433b4b730c0ed18ea
Xinya Zhu <xinya.zhu@aspose.com>
2026-03-13 15:45:35 +0800

    add main func

--- Example/Python/Introduction/CheckVersionNumber.py
+++ Example/Python/Introduction/CheckVersionNumber.py
@@ -1,8 +1,7 @@
-from aspose import cells
-from aspose.pydrawing import Color
 import os
 from pathlib import Path
 from datetime import datetime
+import aspose.cells as cells
 
 def get_source_directory():
     return Path(__file__).parent / ".." / ".." / "Data" / "01_SourceDirectory"
@@ -11,8 +10,11 @@ def get_output_directory():
     return Path(__file__).parent / ".." / ".." / "Data" / "02_OutputDirectory"
 
 def get_data_dir():
-    return Path(__file__).parent / ".." / ".." / "Data" / "Introduction/CheckVersionNumber"
+    return Path(__file__).parent / ".." / ".." / "Data" / "Introduction" / "CheckVersionNumber"
 
 def run():
     print("Aspose.Cells for Python via .NET Version: " + cells.CellsHelper.get_version())
     print("CheckVersionNumber executed successfully.\r\n")
+
+if __name__ == "__main__":
+    run()
--- Example/Python/Introduction/FirstApplication.py
+++ Example/Python/Introduction/FirstApplication.py
@@ -1,21 +1,36 @@
 import os
+from pathlib import Path
 from datetime import datetime
 from aspose.cells import Workbook, SaveFormat
-from pathlib import Path
+from aspose.pydrawing import Color
 
 def get_output_directory():
     return Path(__file__).parent / ".." / ".." / "Data" / "02_OutputDirectory"
 
 def run():
+    # Output directory
     output_dir = get_output_directory()
     
+    # Ensure output directory exists
+    os.makedirs(output_dir, exist_ok=True)
+    
+    # Instantiate a Workbook object that represents Excel file.
     wb = Workbook()
     
+    # When you create a new workbook, a default "Sheet1" is added to the workbook.
     sheet = wb.worksheets[0]
     
+    # Access the "A1" cell in the sheet.
     cell = sheet.cells.get("A1")
     
+    # Input the "Hello World!" text into the "A1" cell
     cell.put_value("Hello World!")
     
+    # Save the Excel file.
     output_path = os.path.join(output_dir, "outputFirstApplication.xlsx")
     wb.save(output_path, SaveFormat.XLSX)
+    
+    print("FirstApplication executed successfully.\r\n")
+
+if __name__ == "__main__":
+    run()
